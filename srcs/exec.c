e0d6fb08ff070e229ec1b5a177c68d0375a7164d
Ramez Shatra <ramezshatra7@gmail.com>
2024-07-03 22:40:30 +0200

    I added some comments to explain what I did

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: rshatra <rshatra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 08:46:05 by rshatra           #+#    #+#             */
-/*   Updated: 2024/07/03 22:05:51 by rshatra          ###   ########.fr       */
+/*   Updated: 2024/07/03 22:38:48 by rshatra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,22 @@ void	delete_node(t_line_data **data, t_line_data *tmp)
 	t_line_data	*tmp2;
 
 	tmp2 = *data;
-	if (tmp == *data)
+	if (tmp == *data) // if the node  we want to delete is the first node
 	{
-		*data = tmp->next;
-		free(tmp);
+		*data = tmp->next; // move to the next node
+		free(tmp);	// free the node we need to delete
 	}
 	else
 	{
-		while (tmp2->next != tmp)
-			tmp2 = tmp2->next;
+		while (tmp2->next != tmp) //as long as we don't reach the node we want to delete
+			tmp2 = tmp2->next; // move to the next node
 		tmp2->next = tmp->next;
 		free(tmp);
 	}
 }
 
-
+// this function will find the path of the command
+// it is just from pipe_x project
 char	*find_path(char *cmd, char **env)
 {
 	char	*paths;
@@ -98,6 +99,26 @@ void	exec_command(t_line_data *line_data, char **env)
 
 void	merage_free_command(t_line_data **data, int len)
 {
+
+/*
+	if we enter the following command:
+	cat < file -v -n
+	we will have the following nodes:
+	node1:		"cat"			type = 0
+	node2:		"<"				type = 5
+	node3:		"file"			type = 7
+	node4:		"-v"	"-n"	type = 0
+
+	we need to merge the command and the arguments in one node
+	so first we will move from node to node search for the type 0
+	we will put all the data in one string
+	str = "cat -v -n"
+	then we will split the string by space and put the data in the nodes
+	node:	"cat"		"-v"		"-n"		type = 0
+			cmd			arg[0]		arg[1]
+
+*/
+
 	t_line_data	*tmp;
 	int			i;
 	char		*str;
@@ -105,57 +126,66 @@ void	merage_free_command(t_line_data **data, int len)
 
 	tmp = *data;
 	str = NULL;
-	str = (char *)ft_malloc(len + 1);
+	str = (char *)ft_malloc(len + 1); // we put everything in one string
 	while (tmp != NULL)
 	{
 		if (tmp->type == 0)
 		{
-			if (str != NULL)
+			if (str != NULL) // if the string is not empty that means we
+						// have data from the previous node so we need to add space
 				ft_strlcat(str, " ", ft_strlen(str) + 2);
 			i = 0;
 			while (tmp->command[i] != NULL)
 			{
+				// below we add the command and the arguments from the current node to the string
 				ft_strlcat(str, tmp->command[i], ft_strlen(str) + ft_strlen(tmp->command[i]) + 1);
-				if (tmp->command[i + 1] != NULL)
+				if (tmp->command[i + 1] != NULL) // if there is another argument we need to add space
 					ft_strlcat(str, " ", ft_strlen(str) + 2);
 				i++;
 			}
 			tmp_delete = tmp;
+			// delete the current node because now we have all the data in str
 			ft_free(tmp->command, NULL, NULL);
 			delete_node(data, tmp_delete);
 		}
-		tmp = tmp->next;
+		tmp = tmp->next; // repeat the process for the next node
 	}
+	// now all the data from all nodes type command is in str
+	// now we need to split the string by space and put the data in the nodes
+	// and we already have the function for that
+	// so command_fill() will split and initialize the node we need
 	command_fill(str, 0, data);
 }
 
 void	command_merage(t_line_data **data)
 {
-	// in this function we will merge the command and the arguments in the
-	// first command node
-	// and we will free the other nodes
+	// in this function we will merge the command and all arguments in the
+	// one new node
+	// and we will free alllll the other nodes
 	t_line_data	*tmp;
 	int			i;
 	int			j;
+							// we will put everythig in one string
+							// then we will split it by space
 	int			space_counter; // ot count how many spaces we need to separate the command and the arguments
-	int			char_counter;
+	int			char_counter; // to count how many characters we need to allocate memory for
 
 	j = 0;
-	space_counter = -1; // if one command we don't need to add space
+	space_counter = -1; // -1 because if one command we don't need to add space
 	char_counter = 0;
 	tmp = *data;
 	while (tmp != NULL)
 	{
 		i = 0;
-		if (tmp->type == 0)
+		if (tmp->type == 0) // if the current node is a command
 		{
 			while (tmp->command[i] != NULL)
 			{
 				j = 0;
-				space_counter++;
+				space_counter++; // we need to add space after each argument
 				while (tmp->command[i][j++] != '\0')
-					char_counter++;
-				i++;
+					char_counter++; // count the number of characters in the command and the arguments
+				i++; // move to the next argument in this node
 			}
 		}
 		tmp = tmp->next;
