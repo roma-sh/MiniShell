953a5176c316ab4c6520756f567e1df4c572184b
Ramez Shatra <ramezshatra7@gmail.com>
2024-07-10 17:45:53 +0200

    file heredoc norminette done

--- srcs/heredoc_handle.c
+++ srcs/heredoc_handle.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_handle.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rshatra <rshatra@student.42.fr>            +#+  +:+       +#+        */
+/*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:34:32 by rshatra           #+#    #+#             */
-/*   Updated: 2024/07/08 17:16:23 by rshatra          ###   ########.fr       */
+/*   Updated: 2024/07/10 17:45:33 by eperperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int after_redi_len(char *line, int i)
+int	after_redi_len(char *line, int i)
 {
 	int		len;
 
 	len = 0;
-	while(line[i] != ' ' && line[i] != '\0' && line[i] != '|'
-					&& line[i] != '<' && line[i] != '>')
+	while (line[i] != ' ' && line[i] != '\0' && line[i] != '|'
+		&& line[i] != '<' && line[i] != '>')
 	{
 		i++;
 		len++;
@@ -28,11 +28,12 @@ int after_redi_len(char *line, int i)
 
 int	heredoc_init(char *line, int i, t_line_data **data)
 {
-	int	fd;
-	t_line_data *new_line_data;
-	char *file = "Libft/tmp_file";
-	char *in_put;
+	int			fd;
+	t_line_data	*new_line_data;
+	char		*file;
+	char		*in_put;
 
+	file = "Libft/tmp_file";
 	new_line_data = (t_line_data *)ft_malloc(sizeof(t_line_data));
 	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd == -1)
@@ -40,7 +41,7 @@ int	heredoc_init(char *line, int i, t_line_data **data)
 		//error_handle function
 	}
 	in_put = readline(">");
-	while (ft_strncmp(in_put, &line[i], after_redi_len(line, i))!= 0)
+	while (ft_strncmp(in_put, &line[i], after_redi_len(line, i)) != 0)
 	{
 		write (fd, in_put, ft_strlen(in_put));
 		write (fd, "\n", 1);
